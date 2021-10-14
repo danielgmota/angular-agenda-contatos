0c6d341f5c258b6864489cb4b4bd8b4c7540f47f
Daniel Mota <guimaraes15@gmail.com>
2021-10-14 17:50:04 -0300

    feat: implemented UpdateContact

--- src/app/contact-update/contact-update.component.ts
+++ src/app/contact-update/contact-update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { ContactsService } from '../contacts.service';
 
 @Component({
   selector: 'app-contact-update',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactUpdateComponent implements OnInit {
 
-  constructor() { }
+  id: any;
+  contactForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    phone: new FormControl('', Validators.required),
+  });
+
+  constructor(private contactsService: ContactsService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    // this.id = this.route.snapshot.params['id'] || null;
+    this.route.paramMap.subscribe(paramMap => {
+      this.id = paramMap.get('id');
+
+      this.contactsService.retrieveContacts(this.id).subscribe(contact => {
+        this.contactForm.reset(contact);
+      },
+      (error) => {
+        console.log(error);
+      });
+    });
   }
 
+  updateContact(){
+    this.contactsService.updateContact({id: this.id, ...this.contactForm.value})
+    .subscribe(contact => {
+      this.contactForm.reset(contact);
+    })
+  }
 }
